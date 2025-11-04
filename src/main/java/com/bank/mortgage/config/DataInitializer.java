d1724495887396333a4bce60d0a47a28206e0fa1
sarokris <saravananand.k18@gmail.com>
2025-11-04 23:55:14 +0100

    Handled the data integrity exception while doing data load on startup

--- src/main/java/com/bank/mortgage/config/DataInitializer.java
+++ src/main/java/com/bank/mortgage/config/DataInitializer.java
@@ -2,8 +2,10 @@ package com.bank.mortgage.config;
 
 import com.bank.mortgage.entity.InterestRates;
 import com.bank.mortgage.repo.InterestRateRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -12,13 +14,11 @@ import java.util.List;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final InterestRateRepository repository;
 
-    public DataInitializer(InterestRateRepository repository) {
-        this.repository = repository;
-    }
 
     @Override
     public void run(String... args) {
@@ -28,7 +28,11 @@ public class DataInitializer implements CommandLineRunner {
                 , createRate(20, "4.10")
         );
         log.info("data loaded");
-        repository.saveAll(rates);
+        try{
+            repository.saveAll(rates);
+        }catch (DataIntegrityViolationException e){
+            log.error("Exception while dataload on startup",e);
+        }
     }
 
     private InterestRates createRate(int maturityPeriod, String interestRate) {
